0a6b2ea8fdd3b4caa5c1c9298274c45cdcf30728
Danyal Naveed <danyalnaveed.dn@gmail.com>
2020-06-03 06:27:19 +0500

    Changed Profile Image

--- Beer-ECommerce-App-iOS-master/Furniture/Controller/ProfileViewController.swift
+++ Beer-ECommerce-App-iOS-master/Furniture/Controller/ProfileViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import JGProgressHUD
 
 class ProfileViewController: UIViewController {
     
@@ -19,9 +20,12 @@ class ProfileViewController: UIViewController {
     
     static var postsCount : Int = 100
     static var saveCount : Int = 1
-    static var count : Int = 1
+    static var count : Int = 100
+    var profileCell = ProfileCell()
+    var profileImage:UIImage?
     
-    
+    var hud : JGProgressHUD  = JGProgressHUD(style: .dark)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,6 +36,21 @@ class ProfileViewController: UIViewController {
         
         tableView.register(UINib(nibName: "PostsCell", bundle: nil), forCellReuseIdentifier: "PostsCell")
         
+        let imageUrl = Auth.auth().currentUser?.photoURL
+        
+        if imageUrl != nil{
+            DispatchQueue.global().async { [weak self] in
+                if let data = try? Data(contentsOf: imageUrl!) {
+                    if let image = UIImage(data: data) {
+                        DispatchQueue.main.async {
+                            self?.profileImage = image
+                            self?.tableView.reloadData()
+                        }
+                    }
+                }
+            }
+        }
+
         
     }
     
@@ -129,6 +148,46 @@ class ProfileViewController: UIViewController {
         present(imagePicker, animated: true)
     }
     
+    func uploadProfileImage(_ image:UIImage, completion: @escaping (_ url: URL?)->()) {
+           guard let uid = Auth.auth().currentUser?.uid else { return }
+           let storageRef = Storage.storage().reference().child("profileImage").child("\(uid)")
+           
+        let imageData = profileImage?.jpegData(compressionQuality: 0.8)
+           let metaData = StorageMetadata()
+           metaData.contentType = "image/jpeg"
+           storageRef.putData(imageData!, metadata: metaData) { (metaData, error) in
+               if error == nil{
+                   print("success for profile photo")
+                   storageRef.downloadURL(completion: { (url, error) in
+                       completion(url)
+                   })
+               }else{
+                   print("error in save image")
+                   completion(nil)
+               }
+           }
+       }
+    
+    func uploadPic(){
+          guard let image = profileImage else {return}
+          
+          let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+          uploadProfileImage(image) { url in
+              changeRequest?.photoURL = url
+              changeRequest?.commitChanges { (error) in
+                  if error != nil {
+                      print(error?.localizedDescription)
+                  }else{
+                    self.hud.indicatorView = JGProgressHUDSuccessIndicatorView()
+                    self.hud.dismiss(afterDelay: 1, animated: true)
+                      let db = Firestore.firestore()
+                      db.collection("users").document(Auth.auth().currentUser!.uid).updateData(["imageUrl": url?.absoluteString])
+                      
+                  }
+              }
+          }
+      }
+    
     
     
 }
@@ -163,6 +222,11 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource{
                 
             }
             
+            if profileImage != nil {
+                profileCell.imgProfilePic.image = profileImage
+
+            }
+            
             profileCell.btnAddImage.addTarget(self, action: #selector(btnSelectImage(_:)), for: .touchUpInside)
             
             return profileCell
@@ -192,6 +256,15 @@ extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationCo
         picker.dismiss(animated: true)
         
         guard let image = info[.originalImage] as? UIImage else { return }
+        profileCell = self.tableView.cellForRow(at: [0,0]) as! ProfileCell
+        profileImage = image
+
+        profileCell.imgProfilePic.image = image
+        hud.show(in: self.view)
+
+        uploadPic()
+
+        //profileCell.reloadInputViews()
         
     }
 }
